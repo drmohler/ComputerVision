4ca58b2514da22253db2e424b195aa4f161a8df0
David Mohler <drmohler@outlook.com>
2018-02-21 12:49:24 -0700

    angle calcs and report

--- MonoPose/MonoPose.m
+++ MonoPose/MonoPose.m
@@ -5,6 +5,9 @@
 
 clear
 close all
+
+meanvec = zeros(100,1);
+
 %read in the images
 i1=imread('Images\cvClass 023.jpg','jpg');
 i2=imread('Images\cvClass 026.jpg','jpg');
@@ -40,107 +43,145 @@ x3pixmat=[X3'
 x4pixmat=[X4'
     ones(1,mc)];  %convert the points to homogeneous coordinates
 
-%Corruption of data points with gaussian noise
-x1pixcor = x1pixmat;
-%corruption of each coordinate in each point seperately
-for i = 1:mc
-  x1pixcor(1,i) = x1pixmat(1,i)+100*randn(1,1);
-  x1pixcor(2,i) = x1pixmat(2,i)+100*randn(1,1);
-%   x1pixcor(3,i) = x1pixmat(3,i)+10*randn(1,1);
-end
+for j = 1:100
+    %Corruption of data points with gaussian noise
+    x1pixcor = x1pixmat;
+    %corruption of each coordinate in each point seperately
+    for i = 1:mc
+      x1pixcor(1,i) = x1pixmat(1,i)+100*randn(1,1);
+      x1pixcor(2,i) = x1pixmat(2,i)+100*randn(1,1);
+    %   x1pixcor(3,i) = x1pixmat(3,i)+10*randn(1,1);
+    end
 
-%Corrupting all coordinates equally across correspondence points
-%(maintains shape, but is shifted in image plane) 
-% x1pixcor(1,:) = x1pixmat(1,:)+75*randn(1,1);
-% x1pixcor(2,:) = x1pixmat(2,:)+75*randn(1,1);
-% x1pixcor(3,:) = x1pixmat(3,:)+75*randn(1,1);
+    %Corrupting all coordinates equally across correspondence points
+    %(maintains shape, but is shifted in image plane) 
+    % x1pixcor(1,:) = x1pixmat(1,:)+75*randn(1,1);
+    % x1pixcor(2,:) = x1pixmat(2,:)+75*randn(1,1);
+    % x1pixcor(3,:) = x1pixmat(3,:)+75*randn(1,1);
 
-%enter object coordinates for the first 7 corners
-Xomat=[0 lbox lbox lbox 0 0 lbox
-       0 0 hbox hbox hbox 0 0
-       0 0 0 -wbox -wbox -wbox -wbox]; %object coords of the four corners
-Xoh = [Xomat; ones(1,mc)]; %Homogeneous object coordinates
-%find calibration matrices
-[gest1qr,lambda1qr,K1]=monoPoseQR(Xomat,x1pixmat); %find K, depth, and g
-Rest1qr=gest1qr(1:3,1:3);Test1qr=gest1qr(1:3,4); %Extraction of R and T
+    %enter object coordinates for the first 7 corners
+    Xomat=[0 lbox lbox lbox 0 0 lbox
+           0 0 hbox hbox hbox 0 0
+           0 0 0 -wbox -wbox -wbox -wbox]; %object coords of the four corners
+    Xoh = [Xomat; ones(1,mc)]; %Homogeneous object coordinates
+    %find calibration matrices
+    [gest1qr,lambda1qr,K1]=monoPoseQR(Xomat,x1pixmat); %find K, depth, and g
+    Rest1qr=gest1qr(1:3,1:3);Test1qr=gest1qr(1:3,4); %Extraction of R and T
 
-[gest1cor,lambda1cor,K1cor]=monoPoseQR(Xomat,x1pixcor); %find K,depth, & g
-Rest1cor=gest1cor(1:3,1:3);Test1cor=gest1cor(1:3,4); %Extraction of R and T
+    [gest1cor,lambda1cor,K1cor]=monoPoseQR(Xomat,x1pixcor); %find K,depth, & g
+    Rest1cor=gest1cor(1:3,1:3);Test1cor=gest1cor(1:3,4); %Extraction of R and T
 
-[gest2qr,lambda2qr,K2]=monoPoseQR(Xomat,x2pixmat);
-Rest2qr=gest2qr(1:3,1:3);Test2qr=gest2qr(1:3,4);
+    [gest2qr,lambda2qr,K2]=monoPoseQR(Xomat,x2pixmat);
+    Rest2qr=gest2qr(1:3,1:3);Test2qr=gest2qr(1:3,4);
 
-[gest3qr,lambda3qr,K3]=monoPoseQR(Xomat,x3pixmat);
-Rest3qr=gest3qr(1:3,1:3);Test3qr=gest3qr(1:3,4);
+    [gest3qr,lambda3qr,K3]=monoPoseQR(Xomat,x3pixmat);
+    Rest3qr=gest3qr(1:3,1:3);Test3qr=gest3qr(1:3,4);
 
-[gest4qr,lambda4qr,K4]=monoPoseQR(Xomat,x4pixmat);
-Rest4qr=gest4qr(1:3,1:3);Test4qr=gest4qr(1:3,4);
+    [gest4qr,lambda4qr,K4]=monoPoseQR(Xomat,x4pixmat);
+    Rest4qr=gest4qr(1:3,1:3);Test4qr=gest4qr(1:3,4);
 
-PI1 = [K1*Rest1qr K1*Test1qr];
-PI1cor = [K1cor*Rest1cor K1cor*Test1cor];
-PI2 = [K2*Rest2qr K2*Test2qr];
-PI3 = [K3*Rest3qr K3*Test3qr];
-PI4 = [K4*Rest4qr K4*Test4qr];
+    PI1 = [K1*Rest1qr K1*Test1qr];
+    PI1cor = [K1cor*Rest1cor K1cor*Test1cor];
+    PI2 = [K2*Rest2qr K2*Test2qr];
+    PI3 = [K3*Rest3qr K3*Test3qr];
+    PI4 = [K4*Rest4qr K4*Test4qr];
 
-%Estimated pixel coordinates of correspondence points
-x1pixest = zeros(size(x1pixmat));
-x1pixestcor = zeros(size(x1pixmat));
-x2pixest = zeros(size(x2pixmat));
-x3pixest = zeros(size(x3pixmat));
-x4pixest = zeros(size(x4pixmat));
-for i = 1:mc
-    x1pixest(:,i) = (PI1*Xoh(:,i))/lambda1qr(i);
-    x1pixestcor(:,i) = (PI1cor*Xoh(:,i))/lambda1cor(i);
-    x2pixest(:,i) = (PI2*Xoh(:,i))/lambda2qr(i);
-    x3pixest(:,i) = (PI3*Xoh(:,i))/lambda3qr(i);
-    x4pixest(:,i) = (PI4*Xoh(:,i))/lambda4qr(i);
-end 
+    %Estimated pixel coordinates of correspondence points
+    x1pixest = zeros(size(x1pixmat));
+    x1pixestcor = zeros(size(x1pixmat));
+    x2pixest = zeros(size(x2pixmat));
+    x3pixest = zeros(size(x3pixmat));
+    x4pixest = zeros(size(x4pixmat));
+    for i = 1:mc
+        x1pixest(:,i) = (PI1*Xoh(:,i))/lambda1qr(i);
+        x1pixestcor(:,i) = (PI1cor*Xoh(:,i))/lambda1cor(i);
+        x2pixest(:,i) = (PI2*Xoh(:,i))/lambda2qr(i);
+        x3pixest(:,i) = (PI3*Xoh(:,i))/lambda3qr(i);
+        x4pixest(:,i) = (PI4*Xoh(:,i))/lambda4qr(i);
+    end 
 
-%Reconstruction of Object Coordinates from estimates
-Xoest1 = zeros(size(Xomat));
-Xoest1cor = zeros(size(Xomat));
-Xoest2 = zeros(size(Xomat));
-Xoest3 = zeros(size(Xomat));
-Xoest4 = zeros(size(Xomat));
-for i = 1:mc
-   Xoest1(:,i) = Rest1qr'*inv(K1)*(lambda1qr(i)*x1pixmat(:,i)-K1*Test1qr);
-   Xoest1cor(:,i) = Rest1cor'*inv(K1cor)*...
-                        (lambda1cor(i)*x1pixmat(:,i)-K1cor*Test1cor);
-   Xoest2(:,i) = Rest2qr'*inv(K2)*(lambda2qr(i)*x2pixest(:,i)-K2*Test2qr); 
-   Xoest3(:,i) = Rest3qr'*inv(K3)*(lambda3qr(i)*x3pixest(:,i)-K3*Test3qr); 
-   Xoest4(:,i) = Rest4qr'*inv(K4)*(lambda4qr(i)*x4pixest(:,i)-K4*Test4qr); 
-end
+    %Reconstruction of Object Coordinates from estimates
+    Xoest1 = zeros(size(Xomat));
+    Xoest1cor = zeros(size(Xomat));
+    Xoest2 = zeros(size(Xomat));
+    Xoest3 = zeros(size(Xomat));
+    Xoest4 = zeros(size(Xomat));
+    for i = 1:mc
+       Xoest1(:,i) = Rest1qr'*inv(K1)*(lambda1qr(i)*x1pixmat(:,i)-K1*Test1qr);
+       Xoest1cor(:,i) = Rest1cor'*inv(K1cor)*...
+                            (lambda1cor(i)*x1pixmat(:,i)-K1cor*Test1cor);
+       Xoest2(:,i) = Rest2qr'*inv(K2)*(lambda2qr(i)*x2pixest(:,i)-K2*Test2qr); 
+       Xoest3(:,i) = Rest3qr'*inv(K3)*(lambda3qr(i)*x3pixest(:,i)-K3*Test3qr); 
+       Xoest4(:,i) = Rest4qr'*inv(K4)*(lambda4qr(i)*x4pixest(:,i)-K4*Test4qr); 
+    end
 
-%Average error in each direction and total distance
-avXRMSE = (sqrt(sum(((Xomat(1,:)-Xoest1(1,:)).^2)/mc))+...
-    sqrt(sum(((Xomat(1,:)-Xoest2(1,:)).^2)/mc))+...
-    sqrt(sum(((Xomat(1,:)-Xoest3(1,:)).^2)/mc))+...
-    sqrt(sum(((Xomat(1,:)-Xoest4(1,:)).^2)/mc)))/4;
-avYRMSE = (sqrt(sum(((Xomat(1,:)-Xoest1(1,:)).^2)/mc))+...
-    sqrt(sum(((Xomat(2,:)-Xoest2(2,:)).^2)/mc))+...
-    sqrt(sum(((Xomat(2,:)-Xoest3(2,:)).^2)/mc))+...
-    sqrt(sum(((Xomat(2,:)-Xoest4(2,:)).^2)/mc)))/4;
-avZRMSE = (sqrt(sum(((Xomat(1,:)-Xoest1(1,:)).^2)/mc))+...
-    sqrt(sum(((Xomat(3,:)-Xoest2(3,:)).^2)/mc))+...
-    sqrt(sum(((Xomat(3,:)-Xoest3(3,:)).^2)/mc))+...
-    sqrt(sum(((Xomat(3,:)-Xoest4(3,:)).^2)/mc)))/4;
+    %Average error in each direction and total distance
+    avXRMSE = (sqrt(sum(((Xomat(1,:)-Xoest1(1,:)).^2)/mc))+...
+        sqrt(sum(((Xomat(1,:)-Xoest2(1,:)).^2)/mc))+...
+        sqrt(sum(((Xomat(1,:)-Xoest3(1,:)).^2)/mc))+...
+        sqrt(sum(((Xomat(1,:)-Xoest4(1,:)).^2)/mc)))/4;
+    avYRMSE = (sqrt(sum(((Xomat(1,:)-Xoest1(1,:)).^2)/mc))+...
+        sqrt(sum(((Xomat(2,:)-Xoest2(2,:)).^2)/mc))+...
+        sqrt(sum(((Xomat(2,:)-Xoest3(2,:)).^2)/mc))+...
+        sqrt(sum(((Xomat(2,:)-Xoest4(2,:)).^2)/mc)))/4;
+    avZRMSE = (sqrt(sum(((Xomat(1,:)-Xoest1(1,:)).^2)/mc))+...
+        sqrt(sum(((Xomat(3,:)-Xoest2(3,:)).^2)/mc))+...
+        sqrt(sum(((Xomat(3,:)-Xoest3(3,:)).^2)/mc))+...
+        sqrt(sum(((Xomat(3,:)-Xoest4(3,:)).^2)/mc)))/4;
 
-%Mean Distance PRMSE 
-dist1 = sqrt((Xomat(1,:)-Xoest1(1,:)).^2+(Xomat(2,:)-Xoest1(2,:)).^2 ...
-+(Xomat(3,:)-Xoest1(3,:)).^2)
-dist2 = sqrt((Xomat(1,:)-Xoest2(1,:)).^2+(Xomat(2,:)-Xoest2(2,:)).^2 ...
-+(Xomat(3,:)-Xoest2(3,:)).^2)
-dist3 = sqrt((Xomat(1,:)-Xoest3(1,:)).^2+(Xomat(2,:)-Xoest3(2,:)).^2 ...
-+(Xomat(3,:)-Xoest3(3,:)).^2)
-dist4 = sqrt((Xomat(1,:)-Xoest4(1,:)).^2+(Xomat(2,:)-Xoest4(2,:)).^2 ...
-+(Xomat(3,:)-Xoest4(3,:)).^2)
-avDRMSE = (sqrt(sum(dist1/mc))+sqrt(sum(dist1/mc))+sqrt(sum(dist1/mc))...
-    +sqrt(sum(dist1/mc)))/4;
+    %Mean Distance PRMSE 
+    dist1 = sqrt((Xomat(1,:)-Xoest1(1,:)).^2+(Xomat(2,:)-Xoest1(2,:)).^2 ...
+    +(Xomat(3,:)-Xoest1(3,:)).^2);
+    dist2 = sqrt((Xomat(1,:)-Xoest2(1,:)).^2+(Xomat(2,:)-Xoest2(2,:)).^2 ...
+    +(Xomat(3,:)-Xoest2(3,:)).^2);
+    dist3 = sqrt((Xomat(1,:)-Xoest3(1,:)).^2+(Xomat(2,:)-Xoest3(2,:)).^2 ...
+    +(Xomat(3,:)-Xoest3(3,:)).^2);
+    dist4 = sqrt((Xomat(1,:)-Xoest4(1,:)).^2+(Xomat(2,:)-Xoest4(2,:)).^2 ...
+    +(Xomat(3,:)-Xoest4(3,:)).^2);
+    avDRMSE = (sqrt(sum(dist1/mc))+sqrt(sum(dist1/mc))+sqrt(sum(dist1/mc))...
+        +sqrt(sum(dist1/mc)))/4;
 
-dist1cor = sqrt((Xomat(1,:)-Xoest1cor(1,:)).^2+(Xomat(2,:)-Xoest1cor(2,:)).^2 ...
-+(Xomat(3,:)-Xoest1cor(3,:)).^2);
-DRMSEcor = sqrt(sum(dist1cor/mc));
+    dist1cor = sqrt((Xomat(1,:)-Xoest1cor(1,:)).^2+(Xomat(2,:)-Xoest1cor(2,:)).^2 ...
+    +(Xomat(3,:)-Xoest1cor(3,:)).^2);
+    DRMSEcor = sqrt(sum(dist1cor/mc))/4;
+    meanvec(j) = avDRMSE; 
+    
+    %construct vectors between known vertices to check angles
+    v12 = Xoest1cor(:,2)-Xoest1cor(:,1);
+    v16 = Xoest1cor(:,6)-Xoest1cor(:,1);    
+    v21 = -v12; 
+    v23 = Xoest1cor(:,3)-Xoest1cor(:,2);
+    v27 = Xoest1cor(:,7)-Xoest1cor(:,2);
+    v32 = -v23;
+    v34 = Xoest1cor(:,4)-Xoest1cor(:,3);
+    v47 = Xoest1cor(:,7)-Xoest1cor(:,4);
+    v43 = -v34;
+    v45 = Xoest1cor(:,5)-Xoest1cor(:,4);
+    v54 = -v45;
+    v56 = Xoest1cor(:,6)-Xoest1cor(:,5);
+    v61 = -v16;
+    v65 = -v56;
+    v67 = Xoest1cor(:,7)-Xoest1cor(:,6); 
+    v72 = -v27;
+    v74 = -v47;
+    v76 = -v67; 
 
+    Theta216 = atan2d(norm(cross(v16,v12)),dot(v16,v12));
+    Theta127 = atan2d(norm(cross(v27,v21)),dot(v27,v21));
+    Theta327 = atan2d(norm(cross(v27,v23)),dot(v27,v23));
+    Theta234 = atan2d(norm(cross(v32,v34)),dot(v32,v34));
+    Theta347 = atan2d(norm(cross(v43,v47)),dot(v43,v47));
+    Theta547 = atan2d(norm(cross(v45,v47)),dot(v45,v47));
+    Theta456 = atan2d(norm(cross(v54,v56)),dot(v54,v56));
+    Theta567 = atan2d(norm(cross(v65,v67)),dot(v65,v67));
+    Theta167 = atan2d(norm(cross(v61,v67)),dot(v61,v67));
+    Theta672 = atan2d(norm(cross(v72,v76)),dot(v72,v76));
+    Theta674 = atan2d(norm(cross(v74,v76)),dot(v74,v76));
+    Theta724 = atan2d(norm(cross(v72,v74)),dot(v72,v74));
+    
+    faceAngles = [Theta216 Theta127 Theta327]
+    
+avg = sum(meanvec)/100 
 %Images and plotting 
 figure(1)
 clf
@@ -148,8 +189,9 @@ image(i1);
 hold on
 plot(X1(:,1)+j*X1(:,2),'g*')
 scatter(x1pixest(1,:),x1pixest(2,:),'r*') %Pure pixel estimates
-scatter(x1pixestcor(1,:),x1pixestcor(2,:),'b^') %Noise corrupted pixel est.
-legend('Image Points','Estimated Points','Corrupted estimate')
+%Noise corrupted pixel est.
+scatter(x1pixestcor(1,:),x1pixestcor(2,:),'c^','MarkerFaceColor','c') 
+legend('Image Points','Estimated Pure Points','Corrupted Estimate')
 title('Image 1')
 hold off
 
@@ -208,7 +250,8 @@ clf
 pts=1:7;
 hold on
 plot3(Xomat(1,pts),Xomat(2,pts),Xomat(3,pts),'--rh')
-plot3(Xoest1cor(1,pts),Xoest1cor(2,pts),Xoest1cor(3,pts),'--b^')
+plot3(Xoest1cor(1,pts),Xoest1cor(2,pts),Xoest1cor(3,pts),'--b^'...
+    ,'MarkerFaceColor','c')
 view(3)
 axis equal
 title('Reconstructed 3D Box Corners (Noisy)')
